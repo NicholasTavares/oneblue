bee38081334c5f29a8c7e213150936f05fb01018
NicholasTavares <nicholasbalby@hotmail.com>
2022-05-23 22:01:02 -0300

    feat: redirect to home if logged in

--- src/pages/Login/Index.tsx
+++ src/pages/Login/Index.tsx
@@ -3,8 +3,17 @@ import { ContainerGrid } from "../../components/Grid/styles";
 import { LayoutLogo, ContainerImg, LogoImg, LayoutForm } from "./styles";
 import Logo from "../../img/Logo.png";
 import LoginForm from "../../components/Forms/Login/Index";
+import { useNavigate } from "react-router-dom";
 
 const Login: React.FunctionComponent = () => {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    if (localStorage.getItem("@user")) {
+      navigate("/home");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <ContainerGrid>
       <LayoutLogo>
