17012ae7e35423e24aefab85e5529dee4df5f8df
NicholasTavares <nicholasbalby@hotmail.com>
2022-05-23 22:00:24 -0300

    feat: register page

--- src/pages/Register/Index.tsx
+++ src/pages/Register/Index.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { ContainerGrid } from "../../components/Grid/styles";
+import { LayoutLogo, ContainerImg, LogoImg, LayoutForm } from "./styles";
+import Logo from "../../img/Logo.png";
+import RegisterForm from "../../components/Forms/Register/Index";
+import { useNavigate } from "react-router-dom";
 
-const Register: React.FunctionComponent = () => <div>Registrar!</div>;
+const Register: React.FunctionComponent = () => {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    if (localStorage.getItem("@user")) {
+      navigate("/home");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <ContainerGrid>
+      <LayoutLogo>
+        <ContainerImg>
+          <LogoImg src={Logo} alt="Logotipo One Blue" />
+        </ContainerImg>
+      </LayoutLogo>
+      <LayoutForm>
+        <RegisterForm />
+      </LayoutForm>
+    </ContainerGrid>
+  );
+};
 
 export default Register;
---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const LayoutLogo = styled.div`
+  background-image: linear-gradient(to left top, #0583F2, #023969);
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 3em;
+  height: 100vh;
+`
+export const ContainerImg = styled.span`
+  display: block;
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: white;
+`
+
+export const LogoImg = styled.img`
+  width: 32em;
+`
+
+export const LayoutForm = styled.div`
+  background-color: #fbfbfb;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+`
